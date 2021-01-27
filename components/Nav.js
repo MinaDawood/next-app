3129556d06462da7a43e1ea3ae860cdc6c11eaef
minadawood <mina.ziden@hotmail.com>
2021-01-27 07:35:43 +0200

    Adding components dir

---
+++ components/Nav.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+import NavStyles from '../styles/Nav.module.css';
+
+const Nav = () => {
+  return (
+    <div className={NavStyles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
